60674b01c25b2a3fec82e7678e2e5dc88fc58c0b
MarcoA09 <marcoa.marquez06@gmail.com>
2025-03-30 19:21:09 -0600

    Update reserves.js

--- src/api/reserves.js
+++ src/api/reserves.js
@@ -1,8 +1,8 @@
 import axios from "./axios";
 
-export const reservedRequest = async (dataToSend) => axios.post(`/createreservation`, dataToSend);
+export const reservedRequest = async (dataToSend) => axios.post(`/api/createreservation`, dataToSend);
 
-export const getReservesDates = async () => axios.get(`/reservations/dates`);
+export const getReservesDates = async () => axios.get(`/api/reservations/dates`);
 
-export const getReservesUser = async (id) => axios.get(`/reserves/${id}`);
+export const getReservesUser = async (id) => axios.get(`/api/reserves/${id}`);
 
